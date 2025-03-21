00b81a9e4dc9e4a4814048a044abb320eecbecb9
Berthet <lu.berthet@gmail.com>
2025-03-21 16:51:41 +0100

    add service

--- src/main/java/com/ipi/mesi_backend_rpg/controller/TagController.java
+++ src/main/java/com/ipi/mesi_backend_rpg/controller/TagController.java
@@ -1,14 +1,11 @@
 package com.ipi.mesi_backend_rpg.controller;
 
 import com.ipi.mesi_backend_rpg.dto.TagDTO;
-import com.ipi.mesi_backend_rpg.model.Tag;
-import com.ipi.mesi_backend_rpg.repository.TagRepository;
-import com.ipi.mesi_backend_rpg.service.TagMapperService;
+import com.ipi.mesi_backend_rpg.service.TagService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,92 +13,52 @@ import java.util.List;
 @RequestMapping("/api/tags")
 public class TagController {
 
-    TagRepository tagRepository;
-    TagMapperService tagMapperService;
+    TagService tagService;
 
-    public TagController(TagRepository tagRepository, TagMapperService tagMapperService) {
-        this.tagRepository = tagRepository;
-        this.tagMapperService = tagMapperService;
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
     }
 
     @GetMapping
     public ResponseEntity<List<TagDTO>> getAllTags() {
-        List<TagDTO> tags = tagRepository.findAll().stream().map(tagMapperService::toDTO).toList();
+        List<TagDTO> tags = tagService.getAllTags();
         return new ResponseEntity<>(tags, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TagDTO> getTagById(@PathVariable Integer id) {
-        Tag tag = tagRepository.findById(id).orElse(null);
-
-        if (tag == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<>(tagMapperService.toDTO(tag), HttpStatus.OK);
+        TagDTO tag = tagService.getTagById(id);
+        return new ResponseEntity<>(tag, HttpStatus.OK);
     }
 
     @GetMapping("/name/{name}")
     public ResponseEntity<TagDTO> getTagByName(@PathVariable String name) {
-
-        Tag tag = tagRepository.findByName(name);
-
-        if (tag == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-
-        tagMapperService.toDTO(tag);
-        return new ResponseEntity<>(tagMapperService.toDTO(tag), HttpStatus.OK);
+        TagDTO tag = tagService.getTagByName(name);
+        return new ResponseEntity<>(tag, HttpStatus.OK);
     }
 
     @GetMapping("/search/{query}")
     public ResponseEntity<List<TagDTO>> searchTags(@PathVariable String query) {
-        List<Tag> tags = tagRepository.findSearchTag(query);
-
-        if (tags.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
-        }
-
-        List<TagDTO> foundTags = tags.stream().map(tagMapperService::toDTO).toList();
-        return new ResponseEntity<>(foundTags, HttpStatus.OK);
+        List<TagDTO> tags = tagService.searchTags(query);
+        return new ResponseEntity<>(tags, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<TagDTO> createTag(@Valid @RequestBody TagDTO tagDTO) {
-        Tag savedTag = tagRepository.save(tagMapperService.toEntity(tagDTO));
-        return new ResponseEntity<>(tagMapperService.toDTO(savedTag), HttpStatus.CREATED);
+        TagDTO tag = tagService.createTag(tagDTO);
+        return new ResponseEntity<>(tag, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<TagDTO> deleteTag(@PathVariable Integer id) {
-
-        Tag tag = tagRepository.findById(id).orElse(null);
-
-        if (tag == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-        tagRepository.delete(tag);
+    public ResponseEntity<Void> deleteTag(@PathVariable Integer id) {
+        tagService.deleteTag(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<TagDTO> updateTag(@PathVariable Integer id, @Valid @RequestBody TagDTO tagDTO) {
-        Tag tag = tagRepository.findById(id).orElse(null);
-
-        if (tag == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-
-        if (!tag.getId().equals(tagDTO.id())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-        
-        Tag updatedTag = tagMapperService.toEntity(tagDTO);
-        updatedTag.setId(tag.getId());
-        tagRepository.save(updatedTag);
-
-        return new ResponseEntity<>(tagMapperService.toDTO(updatedTag), HttpStatus.OK);
+        TagDTO updatedTag = tagService.updateTag(id, tagDTO);
+        return new ResponseEntity<>(updatedTag, HttpStatus.OK);
     }
-
-
+    
 }
--- src/main/java/com/ipi/mesi_backend_rpg/mapper/TagMapper.java
+++ src/main/java/com/ipi/mesi_backend_rpg/mapper/TagMapper.java
@@ -1,12 +1,11 @@
-package com.ipi.mesi_backend_rpg.service;
+package com.ipi.mesi_backend_rpg.mapper;
 
 import com.ipi.mesi_backend_rpg.dto.TagDTO;
 import com.ipi.mesi_backend_rpg.model.Tag;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TagMapperService {
-
+public class TagMapper {
     public TagDTO toDTO(Tag tag) {
         return new TagDTO(
                 tag.getId(),
