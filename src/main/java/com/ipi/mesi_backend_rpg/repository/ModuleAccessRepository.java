5c91d920fbbe2f8c53e4cde4cf119b9f9bafe98d
Berthet <lu.berthet@gmail.com>
2025-03-22 11:24:44 +0100

    add service and mapper to ModuleAccess

---
+++ src/main/java/com/ipi/mesi_backend_rpg/repository/ModuleAccessRepository.java
@@ -0,0 +1,7 @@
+package com.ipi.mesi_backend_rpg.repository;
+
+import com.ipi.mesi_backend_rpg.model.ModuleAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleAccessRepository extends JpaRepository<ModuleAccess, Integer> {
+}
