329dd5d5b5fd583acdc5e7a41edcafe4694d6dbc
DENNER <charlesdenner12@gmail.com>
2025-04-18 00:43:47 +0200

    suppr @notblank

--- src/main/java/com/ipi/mesi_backend_rpg/dto/UserFolderDTO.java
+++ src/main/java/com/ipi/mesi_backend_rpg/dto/UserFolderDTO.java
@@ -1,11 +1,10 @@
 package com.ipi.mesi_backend_rpg.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserFolderDTO(
         Long folderId,
-        @NotNull @NotBlank(message = "userId ne doit pas être null") Long userId,
+        @NotNull(message = "userId ne doit pas être null") Long userId,
         String name,
         Long parentFolder) {
 }
--- src/main/java/com/ipi/mesi_backend_rpg/dto/UserSavedModuleDTO.java
+++ src/main/java/com/ipi/mesi_backend_rpg/dto/UserSavedModuleDTO.java
@@ -1,13 +1,12 @@
 package com.ipi.mesi_backend_rpg.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserSavedModuleDTO(
                 Long savedModuleId,
-                @NotNull @NotBlank(message = "userId ne doit pas être null") Long userId,
-                @NotNull @NotBlank(message = "moduleId ne doit pas être null") Long moduleId,
-                @NotNull @NotBlank(message = "moduleVersionId ne doit pas être null") Long moduleVersionId,
+                @NotNull(message = "userId ne doit pas être null") Long userId,
+                @NotNull(message = "moduleId ne doit pas être null") Long moduleId,
+                @NotNull(message = "moduleVersionId ne doit pas être null") Long moduleVersionId,
                 Long folderId,
                 String alias) {
 }
